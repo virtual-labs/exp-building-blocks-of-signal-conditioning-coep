6f110a5bf3baeec2062d9df1cdf37883e608c14d
vleadadmin <admin@vlabs.ac.in>
2024-09-27 13:03:56 +0000

    https://virtual-labs.github.io/virtual-labs/exp-building-blocks-of-signal-conditioning-coep click on the link to test your code.

---
+++ procedure.md
@@ -0,0 +1,17 @@
+### Procedure 
+
+### Configuration:-
+- Select the sensor for signal conditioning .
+- click on next level 
+
+### Basic Knowledge - 
+- Answer all the questions and click on next level.
+
+### Simulation -
+- Click on the component symbols required for the circuit connection.
+- After clicking the symbol, it will display circults.
+-  Click on the "RED" connect dots to correct next compoent "RED" point.
+-  Connecting points are connected with line and connecting points turn to "Green" shows conection is successfull.
+-  Repeat the same procedure and complete the connection diagram.
+
+
---
+++ simulation/src/questionJson.js
@@ -0,0 +1,145 @@
+
+
+QuestionsJSON = {
+		"data": {
+			"SEC": [{
+
+				"QUES": [{
+					
+					"QC": "What is the full form of FIR in terms of signal filtering?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Filthy impulse response"
+					}, {
+						"ANSID": false,
+						"content": "Finite impulse recombination"
+					}, {
+						"ANSID": false,
+						"content": "Finite impulse reduction"
+					},{
+						"ANSID": true,
+						"content": "Finite impulse response "
+					}]
+				}]
+			}, {
+
+				"QUES": [{
+					
+					"QC": " Which frequency is attenuated in a Low-Pass filter? ",
+					"ANS": [{
+						"ANSID": false,
+						"content": "No frequency"
+					}, {
+						"ANSID": false,
+						"content": "Mid-Range frequency "
+					}, {
+						"ANSID": true,
+						"content": "High frequency "
+					}, {
+						"ANSID": false,
+						"content": "Low frequency"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Piezo-electric Materials are ",
+					"ANS": [{
+						"ANSID": true,
+						"content": "Both mentioned"
+					}, {
+						"ANSID": false,
+						"content": "Naturally available"
+					}, {
+						"ANSID": false,
+						"content": "Aritificially Manufactured"
+					}, {
+						"ANSID": false,
+						"content": "None"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Naturally available pizeoelectric among below",
+					"ANS": [{
+						"ANSID": true,
+						"content": "All mentioned"
+					}, {
+						"ANSID": false,
+						"content": "Quartz"
+					}, {
+						"ANSID": false,
+						"content": "Rubber"
+					}, {
+						"ANSID": false,
+						"content": "Hair"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Which filter contains entirely passive elements? ",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Digital filter"
+					}, {
+						"ANSID": false,
+						"content": "Mechanical filter"
+
+
+					}, {
+						"ANSID": true,
+						"content": "Electrical filter "
+					}, {
+						"ANSID": false,
+						"content": "None"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Which signal is obtained after integrating the unit step signal?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Parabolic Signal"
+					}, {
+						"ANSID": true,
+						"content": "Ramp Signal "
+					}, {
+						"ANSID": false,
+						"content": "Square signal"
+					}, {
+						"ANSID": false,
+						"content": "Unit Step Signal"
+					}]
+					
+				}]
+			}
+			]
+		}
+
+
+	}
+
+
+
+var ANSWERJSON = {
+			
+				"0" : "All the points mentioned",
+				"1" : "Set the standard output for the required limits of process parameter",
+				"2" : "Set majority of the points of the transmitter output in the operating region",
+				"3" : "Pump discharge flow and Outlet flow",
+				"4" : "Nothing is required to be changed",
+				"5" : "Nil"
+				
+			
+}
